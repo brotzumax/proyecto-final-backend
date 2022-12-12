3881844935084ba97463d9cf2fbc2653bf4f918a
Maximiliano <brotzumaxx@gmail.com>
2022-12-12 16:59:49 -0300

    Archivos de ruta exportados por modulos

---
+++ routes/routerCarritos.js
@@ -0,0 +1,115 @@
+const express = require('express');
+const { Router } = express;
+const fs = require('fs');
+const routerCarritos = Router();
+
+let carritos = [];
+cargarCarritos();
+
+//Obtener IDs
+function getLastIdCarrito() {
+    if (carritos.length === 0) {
+        return 0;
+    }
+
+    const lastId = carritos[(carritos.length - 1)].id;
+    return lastId;
+}
+
+
+//Lógica interna
+function getCartById(id) {
+    const carrito = carritos.find(carrito => carrito.id === id);
+    return carrito;
+}
+
+function deleteCartById(id) {
+    const carrito = getCartById(id);
+    const index = carritos.indexOf(carrito);
+    carritos.splice(index, 1);
+}
+
+function insertCartProducts(id, productos) {
+    const cart = getCartById(id);
+    const index = carritos.indexOf(cart);
+    cart.productos = cart.productos.concat(productos);
+    carritos[index] = cart;
+}
+
+function replaceCartProducts(id, productos) {
+    const cart = getCartById(id);
+    const index = carritos.indexOf(cart);
+    cart.productos = productos;
+    carritos[index] = cart;
+}
+
+
+//Persistencia de datos
+function guardarCarritos() {
+    fs.writeFileSync("./persistencia/carritos.json", JSON.stringify(carritos, null, 2));
+}
+
+function cargarCarritos() {
+    carritos = JSON.parse(fs.readFileSync("./persistencia/carritos.json", "utf-8"));
+}
+
+
+//Middlewares
+function cartExist(req, res, next) {
+    if (!carritos.some(carrito => carrito.id === Number(req.params.id))) {
+        res.json({ error: "Carrito no encontrado" })
+    } else {
+        next();
+    }
+}
+
+
+//Métodos carritos
+routerCarritos.post("/", (req, res) => {
+    cargarCarritos();
+    const lastId = getLastIdCarrito() + 1;
+    const nuevoCarrito = { id: lastId, timestamp: Date.now(), productos: [] };
+    carritos.push(nuevoCarrito);
+    guardarCarritos();
+    res.json({ id: lastId });
+});
+
+routerCarritos.delete("/:id", cartExist, (req, res) => {
+    cargarCarritos();
+    const id = Number(req.params.id);
+    deleteCartById(id);
+    guardarCarritos();
+    res.json({ status: "Carrito eliminado" });
+});
+
+routerCarritos.get("/:id/productos", cartExist, (req, res) => {
+    cargarCarritos();
+    const id = Number(req.params.id);
+    const carritoBuscado = getCartById(id);
+    res.json(carritoBuscado.productos);
+});
+
+routerCarritos.post("/:id/productos", cartExist, (req, res) => {
+    cargarCarritos();
+    const id = Number(req.params.id);
+    const productos = req.body;
+    insertCartProducts(id, productos);
+    guardarCarritos();
+    res.json(productos);
+});
+
+routerCarritos.delete("/:id/productos/:id_prod", cartExist, (req, res) => {
+    cargarCarritos();
+    const idCart = Number(req.params.id);
+    const idProduct = Number(req.params.id_prod);
+
+    const productos = getCartById(idCart).productos;
+    const producto = productos.find(producto => producto.id === idProduct);
+    const productIndex = productos.indexOf(producto);
+    productos.splice(productIndex, 1);
+    replaceCartProducts(idCart, productos);
+    guardarCarritos();
+    res.json({ status: `Producto id:${idProduct} eliminado` });
+});
+
+module.exports = { routerCarritos };
---
+++ routes/routerProductos.js
@@ -0,0 +1,110 @@
+const express = require('express');
+const { Router } = express;
+const fs = require('fs');
+const routerProductos = Router();
+
+let administrador = true;
+let productos = [];
+
+
+//Obtener IDs
+function getLastId() {
+    if (productos.length === 0) {
+        return 0;
+    }
+
+    const lastId = productos[(productos.length - 1)].id;
+    return lastId;
+}
+
+
+//Lógica interna
+function getProductById(id) {
+    const producto = productos.find(producto => producto.id === id);
+    return producto;
+}
+
+function deleteProductById(id) {
+    const producto = getProductById(id);
+    const index = productos.indexOf(producto);
+    productos.splice(index, 1);
+}
+
+function replaceProduct(id, nuevoProducto) {
+    let producto = getProductById(id);
+    const index = productos.indexOf(producto);
+    producto = nuevoProducto;
+    producto.id = id;
+    productos[index] = producto;
+}
+
+
+//Persistencia de datos
+function guardarProductos() {
+    fs.writeFileSync("./persistencia/productos.json", JSON.stringify(productos, null, 2));
+}
+
+function cargarProductos() {
+    productos = JSON.parse(fs.readFileSync("./persistencia/productos.json", "utf-8"));
+}
+
+
+//Middlewares
+function productExist(req, res, next) {
+    if (!productos.some(product => product.id === Number(req.params.id))) {
+        res.json({ error: "Producto no encontrado" })
+    } else {
+        next();
+    }
+}
+
+function onlyAdministrator(req, res, next) {
+    if (administrador) {
+        next()
+    } else {
+        res.json({ error: -1, descripcion: "Ruta no autorizada" });
+    }
+}
+
+
+//Métodos productos
+routerProductos.get("/", (req, res) => {
+    cargarProductos();
+    res.json(productos);
+});
+
+routerProductos.get("/:id", productExist, (req, res) => {
+    cargarProductos();
+    const id = Number(req.params.id);
+    const productoBuscado = getProductById(Number(id));
+    res.json(productoBuscado);
+});
+
+routerProductos.post("/", onlyAdministrator, (req, res) => {
+    cargarProductos();
+    const producto = req.body;
+    const idAsignado = getLastId() + 1;
+    producto.id = idAsignado;
+    productos.push(producto);
+    guardarProductos();
+    res.json(producto);
+});
+
+routerProductos.put("/:id", onlyAdministrator, productExist, (req, res) => {
+    cargarProductos();
+    const id = Number(req.params.id);
+    const nuevoProducto = req.body;
+    replaceProduct(id, nuevoProducto);
+    guardarProductos();
+    res.json({ status: "Producto reemplazado" });
+});
+
+routerProductos.delete("/:id", onlyAdministrator, productExist, (req, res) => {
+    cargarProductos();
+    const id = Number(req.params.id);
+    deleteProductById(id);
+    guardarProductos();
+    res.json({ status: "Producto eliminado" });
+});
+
+module.exports = { routerProductos };
