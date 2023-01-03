dfea1882749f229cdf703f1baf1ca002e86c5340
Maximiliano <brotzumaxx@gmail.com>
2023-01-03 09:39:46 -0300

    Modelo de producto corregido

--- models/producto.js
+++ models/producto.js
@@ -1,9 +1,13 @@
 import mongoose from 'mongoose';
 
 const ProductoSchema = new mongoose.Schema({
-    title: { type: String, require: true },
-    thumbnail: { type: String, require: true },
-    price: { type: Number, require: true }
+    timestamp: { type: String, require: true },
+    nombre: { type: String, require: true },
+    descripcion: { type: String, require: true },
+    codigo: { type: String, require: true },
+    foto: { type: String, require: true },
+    precio: { type: Number, require: true },
+    stock: { type: Number, require: true }
 });
 
 export const productos = mongoose.model('productos', ProductoSchema);
